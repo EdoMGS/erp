8bd0ebad2e47540ae3d2aa1e0cadf95fcfffb8b4
Ime Prezime <tvoj@email.hr>
2025-08-08 09:05:18 +0200

    fix: normalize line endings for Python files

--- conftest.py
+++ conftest.py
@@ -1,9 +1,1 @@
-"""
-# conftest.py ensures project root is on sys.path for pytest
-"""
-
-import os
-import sys
-
-# Prepend project root so pytest can import subpackage tests correctly
-sys.path.insert(0, os.getcwd())
+"""# conftest.py ensures project root is on sys.path for pytest"""import osimport sys# Prepend project root so pytest can import subpackage tests correctlysys.path.insert(0, os.getcwd())
