b8108fb85da5a2af970e073bb68b6387ef0db22e
Ime Prezime <tvoj@email.hr>
2025-08-18 11:22:11 +0200

    migrations: merge tenants conflicting 0003 migrations

---
+++ tenants/migrations/0004_merge_0003_alter_tenant_domain_0003_tenantsettings.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("tenants", "0003_alter_tenant_domain"),
+        ("tenants", "0003_tenantsettings"),
+    ]
+
+    operations = []
