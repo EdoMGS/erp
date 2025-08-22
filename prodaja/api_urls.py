58436f76e14a6d08440d333014046074ef4e993c
EdoMGS <edo@mgs-grupa.com>
2025-08-22 08:49:36 +0200

    Update api_urls.py

--- prodaja/api_urls.py
+++ prodaja/api_urls.py
@@ -1,16 +1,18 @@
-from django.urls import path
-
-from .api import (
-    EstimateView,
-    QuoteAcceptView,
-    QuoteCreateView,
-    QuoteDetailView,
-)
-
-urlpatterns = [
-    path("quotes/estimate", EstimateView.as_view()),
-    path("quotes", QuoteCreateView.as_view()),
-    path("quotes/<int:pk>", QuoteDetailView.as_view()),
-    path("quotes/<int:pk>/send", QuoteSendView.as_view()),
-    path("quotes/<int:pk>/accept", QuoteAcceptView.as_view()),
-]
+-from prodaja.api import (
++from prodaja.api import (
+     QuoteCreateView,
+     QuoteDetailView,
+     QuoteAcceptView,
+     QuoteSendView,
+-    QuoteToWorkOrderView,
++    QuoteRevisionView,   # <- dodaj ovo
+ )
+ 
+ urlpatterns = [
+     path("quotes", QuoteCreateView.as_view()),
+     path("quotes/<int:pk>", QuoteDetailView.as_view()),
+     path("quotes/<int:pk>/send", QuoteSendView.as_view()),
+     path("quotes/<int:pk>/accept", QuoteAcceptView.as_view()),
+-    path("quotes/<int:pk>/to-wo", QuoteToWorkOrderView.as_view()),
++    path("quotes/<int:pk>/revision", QuoteRevisionView.as_view()),  # <- i ovo
+ ]
